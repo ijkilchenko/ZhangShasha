02994fcc0a5ec6956a9b3588ef8a1f092f278add
ijk <ijkilchenko@gmail.com>
2013-11-04 16:41:42 -0500

    Made changes to Node class with help from stackoverflow; deleted Tree class.

--- src/zhsh/Main.java
+++ src/zhsh/Main.java
@@ -4,20 +4,24 @@
  */
 package zhsh;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         System.out.println("Hello World!");
 
-        String tree1Str= "f(d(a c(b))e)";
+        String tree1Str1= "f(d(a c(b))e)";
+        String tree1Str2= "f(c(d(a b))e)";
 
-        Tree treeFromString= new Tree(tree1Str);
+        Node treeFromString= new Node();
+        treeFromString= treeFromString.parseTree(tree1Str2);
         System.out.println(treeFromString.printTree());
 
     }
 
-    private int ZhangShasha(Tree tree1, Tree tree2){
+/*    private int ZhangShasha(Tree tree1, Tree tree2){
 
         return 1;
-    }
+    }*/
 }
--- src/zhsh/Node.java
+++ src/zhsh/Node.java
@@ -3,22 +3,55 @@
  * Date: 10/26/13
  */
 package zhsh;
+
+import java.io.IOException;
+import java.io.StreamTokenizer;
+import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Node {
-    String label;
-    List<Node> children;
+    private String name;
+    private ArrayList<Node> children = new ArrayList<Node>();
 
     public Node(){
     }
 
-    public Node(String label){
-        this.label= label;
-        children= new ArrayList<Node>();
+    public static Node parseTree(String s) throws IOException {
+        StreamTokenizer tokenizer = new StreamTokenizer(new StringReader(s));
+        tokenizer.nextToken();                      // Move to first token
+        Node result = new Node(tokenizer);     // Parse root node (and children)
+        if (tokenizer.ttype != StreamTokenizer.TT_EOF) {
+            throw new RuntimeException("Leftover token: "+ tokenizer.ttype);
+        }
+        return result;
     }
 
-    public void addChild(Node newNode){
-        this.children.add(newNode);
+    Node(StreamTokenizer tokenizer) throws IOException {
+        if (tokenizer.ttype != StreamTokenizer.TT_WORD) {
+            throw new RuntimeException("identifier expected; got: " + tokenizer.ttype);
+        }
+        name = tokenizer.sval;                  // read and
+        tokenizer.nextToken();                       // consume the name
+        if (tokenizer.ttype == '(') {           // Children?
+            tokenizer.nextToken();                     // Yes, consume '('
+            do {
+                children.add(new Node(tokenizer));  // Add and parse a child
+            } while (tokenizer.ttype != ')');     // Until we reach ')'
+            tokenizer.nextToken();                     // Consume ')'
+        }
+    }
+
+    public String printTree(){
+        String treeString= "";
+        treeString= printTree(this, treeString);
+        return treeString;
+    }
+
+    private String printTree(Node currentRoot, String currentString){
+        currentString += currentRoot.name + " ";
+        for (int i= 0; i < currentRoot.children.size(); i++){
+            currentString= printTree(currentRoot.children.get(i), currentString);
+        }
+        return currentString;
     }
 }
