f99f8c44c1638dfabd2a26bb629b142fd4cf067e
ijk <ijkilchenko@gmail.com>
2013-11-13 23:25:51 -0500

    The structure and most of the algorithm is complete, now to debug...

--- src/zhsh/Main.java
+++ src/zhsh/Main.java
@@ -20,6 +20,7 @@ public class Main {
         Tree tree2= new Tree(tree1Str2);
 
         int distance= ZhangShasha(tree1, tree2);
+        System.out.println(distance);
 
     }
 
@@ -31,15 +32,52 @@ public class Main {
         tree1.keyroots();
         tree2.index();
         tree2.l();
-        tree1.keyroots();
+        tree2.keyroots();
 
         ArrayList<Integer> l1= tree1.l;
         ArrayList<Integer> keyroots1= tree1.keyroots;
         ArrayList<Integer> l2= tree2.l;
         ArrayList<Integer> keyroots2= tree2.keyroots;
 
-        TD = new int[keyroots1.size()][keyroots2.size()];
+        TD = new int[l1.size()+1][l2.size()+1];
 
-        return 1;
+        for (int i1= 1; i1 < keyroots1.size()+1; i1++){
+            for (int j1= 1; j1 < keyroots2.size()+1; j1++){
+                int i= keyroots1.get(i1-1);
+                int j= keyroots2.get(j1-1);
+                TD[i1][j1]= treedist(l1, l2, i, j);
+            }
+        }
+
+        return TD[keyroots1.size()][keyroots2.size()];
+    }
+
+    private static int treedist(ArrayList<Integer> l1, ArrayList<Integer> l2, int i, int j){
+        int[][] forestdist= new int[i+1][j+1];
+
+        int Gamma= 1;
+
+        forestdist[0][0]= 0;
+        for (int i1= l1.get(i-1); i1 <= i; i1++){
+            forestdist[i1][0]= forestdist[i1-1][0] + Gamma;
+        }
+        for (int j1= l2.get(j-1); j1 <= j; j1++){
+            forestdist[0][j1]= forestdist[0][j1-1] + Gamma;
+        }
+        for (int i1= l1.get(i-1); i1 <= i; i1++){
+            for (int j1= l2.get(j-1); j1 <= j; j1++){
+                if (l1.get(i1-1) == l1.get(i-1) && l2.get(j1-1) == l1.get(j-1)){
+                    forestdist[i1][j1]= Math.min(Math.min(forestdist[i1-1][j1] + Gamma, forestdist[i1][j1-1] + Gamma),
+                            forestdist[i1-1][j1-1] + Gamma);
+                    TD[i1][j1]= forestdist[i1][j1];
+                }
+                else{
+                    forestdist[i1][j1]= Math.min(Math.min(forestdist[i1-1][j1] + Gamma, forestdist[i1][j1-1] + Gamma),
+                            forestdist[i1-1][j1-1] + TD[i1][j1]);
+                }
+            }
+        }
+
+        return forestdist[i][j];
     }
 }
