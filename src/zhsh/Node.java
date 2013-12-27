8494d3373381ea283a68db05d313c4f9810efeab
ijk <ijkilchenko@gmail.com>
2013-12-27 14:46:37 -0500

    Updated Node class with comments.

--- src/zhsh/Node.java
+++ src/zhsh/Node.java
@@ -5,15 +5,16 @@
 package zhsh;
 import java.util.ArrayList;
 public class Node {
-    public String label;
-    public int index;
-    public ArrayList<Node> children= new ArrayList<Node>();
-    public Node leftmost;
+    public String label; //Node label. //TODO: Use Java generics. See #1 in README.
+    public int index; //Preorder index.
+    public ArrayList<Node> children= new ArrayList<Node>(); //Note: trees need not be binary.
+    public Node leftmost; //Used by the recursive O(n) leftmost() function in the Tree class.
 
     public Node(){
 
     }
 
+    //TODO: Use Java generics. See #1 in README.
     public Node(String label){
         this.label= label;
     }
